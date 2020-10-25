5e1bb04932a265531a6cdc7f5042c5a5db2c77d8
origho-precious <origho9@gmail.com>
2020-10-25 14:32:49 +0100

    adding styles, links and text to project section

--- src/components/project/index.jsx
+++ src/components/project/index.jsx
@@ -5,26 +5,21 @@ const Project = ({ title, repoUrl, siteUrl, stack, children }) => {
   return (
     <div className={styles.Project}>
       <header className={styles.flexbox}>
-        <i className="fas fa-folder-open"></i>
-        <div>
-          <a href={repoUrl} target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-github"></i>
-          </a>
-          <a href={siteUrl} target="_blank" rel="noopener noreferrer">
-            <i className="fas fa-external-link-alt"></i>
-          </a>
-        </div>
+        <a href={repoUrl} target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href={siteUrl} target="_blank" rel="noopener noreferrer">
+          <i className="fas fa-external-link-alt"></i>
+        </a>
       </header>
-      <h2>{title}</h2>
-      <p>
-        {children}
-      </p>
+      <div className={styles.content}>
+        <h2>{title}</h2>
+        <p>{children}</p>
+      </div>
       <footer>
-        {
-          stack.map((tech, id) => {
-            return <p key={id}>{tech}</p>
-          })
-        }
+        {stack.map((tech, id) => {
+          return <p key={id}>{tech}</p>;
+        })}
       </footer>
     </div>
   );
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -7,30 +7,57 @@ const Projectspage = () => {
       <div className={styles.Projects}>
         <h1>Somethings I've built</h1>
         <section>
-            <Project title='Dizcuzzion' repoUrl="https://" siteUrl="https://" stack={['Algolia', 'PHP', 'Wordpress']}>
-                Building a custom multisite compatible WordPress plugin to build
-                global search with Algolia
-            </Project>
-            <Project title='Dizcuzzion' repoUrl="https://" siteUrl="https://" stack={['Algolia', 'PHP', 'Wordpress']}>
-                Building a custom multisite compatible WordPress plugin to build
-                global search with Algolia
-            </Project>
-            <Project title='Dizcuzzion' repoUrl="https://" siteUrl="https://" stack={['Algolia', 'PHP', 'Wordpress']}>
-                Building a custom multisite compatible WordPress plugin to build
-                global search with Algolia
-            </Project>
-            <Project title='Dizcuzzion' repoUrl="https://" siteUrl="https://" stack={['Algolia', 'PHP', 'Wordpress']}>
-                Building a custom multisite compatible WordPress plugin to build
-                global search with Algolia
-            </Project>
-            <Project title='Dizcuzzion' repoUrl="https://" siteUrl="https://" stack={['Algolia', 'PHP', 'Wordpress']}>
-                Building a custom multisite compatible WordPress plugin to build
-                global search with Algolia
-            </Project>
-            <Project title='Dizcuzzion' repoUrl="https://" siteUrl="https://" stack={['Algolia', 'PHP', 'Wordpress']}>
-                Building a custom multisite compatible WordPress plugin to build
-                global search with Algolia
-            </Project>
+          <Project
+            title="Dizcuzzion"
+            repoUrl="https://github.com/Origho-precious/Realtime-Chat-App"
+            siteUrl="https://dizcuzz.netlify.app/"
+            stack={["React", "Redux", "Firebase"]}
+          >
+            A realtime chat app for muliple user, users are identified by their emails. 
+          </Project>
+          <Project
+            title="Iphone-store"
+            repoUrl="https://github.com/Origho-precious/ecommerce-project"
+            siteUrl="https://iphone-store.netlify.app"
+            stack={["React", "Redux"]}
+          >
+            This is a mini ecommerce App, I built this app to get a better understanding of how Redux.
+          </Project>
+          <Project
+            title="Covid19-stats-app"
+            repoUrl="https://github.com/Origho-precious/Covid-19-Tracker"
+            siteUrl="https://covid19-tracking-app.netlify.app/"
+            stack={["HTML", "CSS", "Javascript"]}
+          >
+            A simple and beautiful app that shows Covid19 stats for a country searched for and also the world stat.
+          </Project>
+          <Project
+            title="Mini Youtube Browser"
+            repoUrl="https://github.com/Origho-precious/Mini-Youtube-Browser"
+            siteUrl="https://mini-youtube-browser.netlify.app"
+            stack={["React", "Youtube Api"]}
+          >
+            An app just like youtube with a search functionality. This app uses
+            the youtube Api.
+          </Project>
+          <Project
+            title="Windbnb - Airbnb clone"
+            repoUrl="https://github.com/Origho-precious/windbnb"
+            siteUrl="https://windbnb-airbnbclone.netlify.app"
+            stack={["React", "Firebase"]}
+          >
+            A mini clone for airbnb for stays in Finland. The data are just
+            random though.
+          </Project>
+          <Project
+            title="Github Jobs"
+            repoUrl="https://github.com/Origho-precious/Github-Jobs-"
+            siteUrl="https://github-jobs-app.netlify.app"
+            stack={["React", "Context Api", "Github Api"]}
+          >
+            An app that enables users to search for Job opening in different
+            locations using the Github Api.
+          </Project>
         </section>
       </div>
     );
