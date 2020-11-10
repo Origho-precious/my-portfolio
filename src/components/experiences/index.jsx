8e34962547b395a85378f5600278d59e2163eede
origho-precious <origho9@gmail.com>
2020-11-10 11:42:52 +0100

    Adding mobile resume page

--- src/components/experiences/index.jsx
+++ src/components/experiences/index.jsx
@@ -6,86 +6,86 @@ class Experiences extends Component{
         show: 0
     }
 
-    renderContent = () => {
-        const { show } = this.state;
-        if(show === 0){
-            return (
-              <div className={styles.experience}>
-                <header>
-                  <h3>
-                    Frontend developer <span>@IjatTechnologies</span>
-                  </h3>
-                  <p>February 2020 - Present</p>
-                </header>
-                <div className={styles.text}>
-                  <ul>
-                    <li>
-                      <i className="fas fa-play"></i>
-                      <p>
-                        Writing modern, and performant code for client projects
-                        and company projects
+  renderContent = () => {
+    const { show } = this.state;
+    if (show === 0) {
+      return (
+        <div className={styles.experience}>
+          <header>
+            <h3>
+              Frontend developer <span>@IjatTechnologies</span>
+            </h3>
+            <p>February 2020 - Present</p>
+          </header>
+          <div className={styles.text}>
+            <ul>
+              <li>
+                <i className="fas fa-play"></i>
+                <p>
+                  Writing modern, and performant code for client projects
+                  and company projects
                       </p>
-                    </li>
-                    <li>
-                      <i className="fas fa-play"></i>
-                      <p>
-                        Working with varieties of Languages, libraries,
-                        platforms and frameworks e.g HTML, CSS(Sass), Bootstrap 4,
-                        Javascript, Reactjs, Redux etc
+              </li>
+              <li>
+                <i className="fas fa-play"></i>
+                <p>
+                  Working with varieties of Languages, libraries,
+                  platforms and frameworks e.g HTML, CSS(Sass), Bootstrap 4,
+                  Javascript, Reactjs, Redux etc
                       </p>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            );
-        } else if (show === 1) {
-            return (
-              <div className={styles.experience}>
-                <header>
-                  <h3>Freelance Frontend developer</h3>
-                  <p>July 2020 - Present</p>
-                </header>
-                <div className={styles.text}>
-                  <ul>
-                    <li>
-                      <i className="fas fa-play"></i>
-                      <p>
-                        I'm a parttime freelancer and I also collaborate with my friends on side projects.
-                      </p>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            );
-        }
-    }
-
-    render() {
-        const { show } = this.state;
-        
-        return (
-          <div className={styles.Experience}>
-            <nav className={styles.sidebar}>
-              <div className={styles.line} />
-              <div>
-                <p
-                  className={show === 0 ? styles.active : null}
-                  onClick={() => this.setState({ show: 0 })}
-                >
-                  Ijat
-                </p>
-                <p
-                  className={show === 1 ? styles.active : null}
-                  onClick={() => this.setState({ show: 1 })}
-                >
-                  Freelancing
-                </p>
-              </div>
-            </nav>
-            <div className={styles.content}>{this.renderContent()}</div>
+              </li>
+            </ul>
           </div>
-        );
-    };
+        </div>
+      );
+    } else if (show === 1) {
+      return (
+        <div className={styles.experience}>
+          <header>
+            <h3>Freelance Frontend developer</h3>
+            <p>July 2020 - Present</p>
+          </header>
+          <div className={styles.text}>
+            <ul>
+              <li>
+                <i className="fas fa-play"></i>
+                <p>
+                  I'm a parttime freelancer and I also collaborate with my friends on side projects.
+                      </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  render() {
+      const { show } = this.state;
+      
+      return (
+        <div className={styles.Experience}>
+          <nav className={styles.sidebar}>
+            <div className={styles.line} />
+            <div>
+              <p
+                className={show === 0 ? styles.active : null}
+                onClick={() => this.setState({ show: 0 })}
+              >
+                Ijat
+              </p>
+              <p
+                className={show === 1 ? styles.active : null}
+                onClick={() => this.setState({ show: 1 })}
+              >
+                Freelancing
+              </p>
+            </div>
+          </nav>
+          <div className={styles.content}>{this.renderContent()}</div>
+        </div>
+      );
+  };
 };
 
 export default Experiences;
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,16 +4,34 @@ import styles from './navbar.module.scss';
 import { useSpring, animated } from 'react-spring';
 
 const Navbar = () => {
-    const [showNav, setShowNav] = useState(false);
+  const [showNav, setShowNav] = useState(false);
 
-    const props = useSpring({
-        marginTop: showNav ? '2rem' : '-500px',
-    })
+  const props = useSpring({
+    marginTop: showNav ? '2rem' : '-500px',
+  });
 
-    return (
-      <div className={styles.Navbar}>
+  return (
+    <div className={styles.Navbar}>
+      <div className={styles.links}>
+        <NavLink activeClassName={styles.active} exact to="/">
+          Me
+        </NavLink>
+        <NavLink activeClassName={styles.active} exact to="/resume">
+          Resume
+        </NavLink>
+        <NavLink activeClassName={styles.active} exact to="/projects">
+          Projects
+        </NavLink>
+        <NavLink activeClassName={styles.active} exact to="/contact">
+          Contact
+        </NavLink>
+        <NavLink activeClassName={styles.active} exact to="/resources">
+          Resources
+        </NavLink>
+      </div>
+      <div className={styles.mobileNav}>
         <animated.div style={props}>
-          <div className={styles.links}>
+          <div className={styles.m_links}>
             <NavLink activeClassName={styles.active} exact to="/">
               Me
             </NavLink>
@@ -31,11 +49,12 @@ const Navbar = () => {
             </NavLink>
           </div>
         </animated.div>
-        <div onClick={() => setShowNav(!showNav)} className={styles.hamburger}>
-          <i className="fas fa-bars"></i>
-        </div>
       </div>
-    );
+      <div onClick={() => setShowNav(!showNav)} className={styles.hamburger}>
+        <i className="fas fa-bars"></i>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
