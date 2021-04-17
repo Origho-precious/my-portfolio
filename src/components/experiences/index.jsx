36e2ce18ca01b73f07abbf563a60abe7d0ffcc14
Origho-precious <origho9@gmail.com>
2021-04-17 09:51:04 +0100

    update: adding Parallel to this portfolio

--- src/components/experiences/index.jsx
+++ src/components/experiences/index.jsx
@@ -1,91 +1,124 @@
-import React, { Component } from 'react';
-import styles from './experiences.module.scss';
+import React, { Component } from "react";
+import styles from "./experiences.module.scss";
 
-class Experiences extends Component{
-    state = {
-        show: 0
-    }
+class Experiences extends Component {
+	state = {
+		show: 0,
+	};
 
-  renderContent = () => {
-    const { show } = this.state;
-    if (show === 0) {
-      return (
-        <div className={styles.experience}>
-          <header>
-            <h3>
-              Frontend developer <span>@IjatTechnologies</span>
-            </h3>
-            <p>February 2020 - Present</p>
-          </header>
-          <div className={styles.text}>
-            <ul>
-              <li>
-                <i className="fas fa-play"></i>
-                <p>
-                  Writing modern, and performant code for client projects
-                  and company projects
-                      </p>
-              </li>
-              <li>
-                <i className="fas fa-play"></i>
-                <p>
-                  Working with varieties of Languages, libraries,
-                  platforms and frameworks e.g HTML, CSS(Sass), Bootstrap 4,
-                  Javascript, Reactjs, Redux etc
-                      </p>
-              </li>
-            </ul>
-          </div>
-        </div>
-      );
-    } else if (show === 1) {
-      return (
-        <div className={styles.experience}>
-          <header>
-            <h3>Freelance Frontend developer</h3>
-            <p>July 2020 - Present</p>
-          </header>
-          <div className={styles.text}>
-            <ul>
-              <li>
-                <i className="fas fa-play"></i>
-                <p>
-                  I'm a parttime freelancer and I also collaborate with my friends on side projects.
-                      </p>
-              </li>
-            </ul>
-          </div>
-        </div>
-      );
-    }
-  };
+	renderContent = () => {
+		const { show } = this.state;
+		if (show === 0) {
+			return (
+				<div className={styles.experience}>
+					<header>
+						<h3>
+							Frontend developer <span>@IjatTechnologies</span>
+						</h3>
+						<p>February 2020 - December 2020</p>
+					</header>
+					<div className={styles.text}>
+						<ul>
+							<li>
+								<i className="fas fa-play"></i>
+								<p>
+									Writing modern, and performant code for client projects and
+									company projects
+								</p>
+							</li>
+							<li>
+								<i className="fas fa-play"></i>
+								<p>
+									Working with varieties of Languages, libraries, platforms and
+									frameworks e.g HTML, CSS(Sass), Bootstrap 4, Javascript,
+									Reactjs, Redux etc
+								</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+			);
+		} else if (show === 1) {
+			return (
+				<div className={styles.experience}>
+					<header>
+						<h3>Frontend Developer</h3>
+						<p>July 2020 - Present</p>
+					</header>
+					<div className={styles.text}>
+						<ul>
+							<li>
+								<i className="fas fa-play"></i>
+								<p>
+									I'm a part-time freelancer and I also collaborate with my
+									friends on side projects.
+								</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+			);
+		} else if (show === 2) {
+			return (
+				<div className={styles.experience}>
+					<header>
+						<h3>
+							Frontend developer <span>@Useparallel</span>
+						</h3>
+						<p>December 2020 - Present</p>
+					</header>
+					<div className={styles.text}>
+						<ul>
+							<li>
+								<i className="fas fa-play"></i>
+								<p>
+									I'm work at{" "}
+									<a href="https://twitter.com/useparallel" trget="_blank">
+										Parallel
+									</a>{" "}
+									as a frontend developer. We work with: Nextjs, Typescript,
+									Tailwindcss and styled-components.
+								</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+			);
+		}
+	};
 
-  render() {
-      const { show } = this.state;
-      
-      return (
-        <div className={styles.Experience}>
-          <nav className={styles.sidebar}>
-            <div className={styles.line} />
-            <div>
-              <p
-                className={show === 0 ? styles.active : null}
-                onClick={() => this.setState({ show: 0 })}
-              >
-                Ijat
-              </p>
-              <p
-                className={show === 1 ? styles.active : null}
-                onClick={() => this.setState({ show: 1 })}
-              >
-                Freelancing
-              </p>
-            </div>
-          </nav>
-          <div className={styles.content}>{this.renderContent()}</div>
-        </div>
-      );
-  };
-};
+	render() {
+		const { show } = this.state;
+
+		return (
+			<div className={styles.Experience}>
+				<nav className={styles.sidebar}>
+					<div className={styles.line} />
+					<div>
+						<p
+							className={show === 0 ? styles.active : null}
+							onClick={() => this.setState({ show: 0 })}
+						>
+							Ijat
+						</p>
+						<p
+							className={show === 1 ? styles.active : null}
+							onClick={() => this.setState({ show: 1 })}
+						>
+							Freelancing
+						</p>
+						<p
+							className={show === 2 ? styles.active : null}
+							onClick={() => this.setState({ show: 2 })}
+						>
+							Parallel
+						</p>
+					</div>
+				</nav>
+				<div className={styles.content}>{this.renderContent()}</div>
+			</div>
+		);
+	}
+}
 
 export default Experiences;
--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -6,67 +6,59 @@ import Education from '../../components/education';
 
 const Resume = () => {
     return (
-      <div className={styles.Resume}>
-        <section className={styles.intro}>
-          <header>
-            <h1>Overview</h1>
-            <a href={resume} download>
-              Download Resume
-            </a>
-          </header>
-          <p>
-            Hello! I'm Origho Precious, a Frontend developer based in
-            Portharcourt, Rivers State, Nigeria. I enjoy creating the frontend
-            of things that live on the internet, whether that be websites,
-            applications, or anything in between. I keep learning new frontend
-            technologies to improve my skillset and build more pixel-perfect,
-            and performant web apps.
-          </p>
-          <p>Here are a few technologies I work with:</p>
-          <ul>
-            <li>
-              <i className="fas fa-play"></i> <p>HTML & CSS(Sass)</p>
-            </li>
-            <li>
-              <i className="fas fa-play"></i>
-              <p>Bootstrap</p>
-            </li>
-            <li>
-              <i className="fas fa-play"></i>
-              <p>Javascript</p>
-            </li>
-            <li>
-              <i className="fas fa-play"></i>
-              <p>Reactjs and Redux</p>
-            </li>
-            <li>
-              <i className="fas fa-play"></i>
-              <p>Firebase</p>
-            </li>
-            <li>
-              <i className="fas fa-play"></i>
-              <p>Nextjs</p>
-            </li>
-            <li>
-              <i className="fas fa-play"></i>
-              <p>Jest & Enzyme</p>
-            </li>
-            <li>
-              <i className="fas fa-play"></i>
-              <p>Apollo Client</p>
-            </li>
-          </ul>
-        </section>
-        <section className={styles.experiences}>
-            <h1>Experiences</h1>
-            <Experiences/>
-        </section>
-        <section className={styles.education}>
-            <h1>Education</h1>
-            <Education/>
-        </section>
-      </div>
-    );
+			<div className={styles.Resume}>
+				<section className={styles.intro}>
+					<header>
+						<h1>Overview</h1>
+						<a href={resume} download>
+							Download Resume
+						</a>
+					</header>
+					<p>
+						Hello! I'm Origho Precious, a Frontend developer based in
+						Portharcourt, Rivers State, Nigeria. I enjoy creating the frontend
+						of things that live on the internet, whether that be websites,
+						applications, or anything in between. I keep learning new frontend
+						technologies to improve my skillset and build more pixel-perfect,
+						and performant web apps.
+					</p>
+					<p>Here are a few technologies I work with:</p>
+					<ul>
+						<li>
+							<i className="fas fa-play"></i> <p>HTML & CSS(Sass)</p>
+						</li>
+						<li>
+							<i className="fas fa-play"></i>
+							<p>TailwindCSS</p>
+						</li>
+						<li>
+							<i className="fas fa-play"></i>
+							<p>Styled-Components</p>
+						</li>
+						<li>
+							<i className="fas fa-play"></i>
+							<p>Javascript</p>
+						</li>
+						<li>
+							<i className="fas fa-play"></i>
+							<p>Reactjs and Redux</p>
+						</li>
+						<li>
+							<i className="fas fa-play"></i>
+							<p>Nextjs</p>
+						</li>
+					</ul>
+				</section>
+				<section className={styles.experiences}>
+					<h1>Experiences</h1>
+					<Experiences />
+				</section>
+				<section className={styles.education}>
+					<h1>Education</h1>
+					<Education />
+				</section>
+			</div>
+		);
 };
 
 export default Resume;
