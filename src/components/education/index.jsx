22b45b7f8240c0f331c0e502942a8cbfdf7439c9
Origho-precious <origho9@gmail.com>
2021-06-20 22:51:49 +0100

    feat: updating education

--- src/components/education/index.jsx
+++ src/components/education/index.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import styles from './education.module.scss';
+import React from "react";
+import styles from "./education.module.scss";
 
 const Education = () => {
-    return (
-      <div className={styles.Education}>
-        <h3>
-            University Of Portharcourt, Rivers state,Nigeria
-        </h3>
-        <h4>BSc Animal And Environmental Biology</h4>
-        <p>January 2017 - Present</p>
-      </div>
-    );
-}
+	return (
+		<div className={styles.Education}>
+			<h3>University Of Portharcourt, Rivers state,Nigeria</h3>
+			<h4>B.Sc Animal And Environmental Biology</h4>
+			<p>January 2017 - August 2021</p>
+		</div>
+	);
+};
 
 export default Education;
