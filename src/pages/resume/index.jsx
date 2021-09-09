1fcde3351d777b6260eb1f0bd1a804ee952a9702
Origho-precious <Origho9@gmail.com>
2021-09-09 08:40:08 +0100

    feat: updating resume page

--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -1,76 +1,81 @@
-import React from 'react';
-import styles from './resume.module.scss';
+import React from "react";
+import styles from "./resume.module.scss";
 import resume from "../../assets/Origho Precious Resume.docx";
-import Experiences from '../../components/experiences';
-import Education from '../../components/education';
+import Experiences from "../../components/experiences";
+import Education from "../../components/education";
 
 const Resume = () => {
-    return (
-			<div className={styles.Resume}>
-				<section className={styles.intro}>
-					<header>
-						<h1>Overview</h1>
-						<a href={resume} download>
-							Download Resume
-						</a>
-					</header>
-					<p>
-						Hello! I'm Origho Precious, a Frontend developer based in
-						Portharcourt, Rivers State, Nigeria. I enjoy creating the frontend
-						of things that live on the internet, whether that be websites,
-						applications, or anything in between. I keep learning new frontend
-						technologies to improve my skillset and build more pixel-perfect,
-						and performant web apps.
-					</p>
-					<p>Here are a few technologies I work with:</p>
-					<ul>
-						<li>
-							<i className="fas fa-play"></i> <p>HTML & CSS(Sass)</p>
-						</li>
-						<li>
-							<i className="fas fa-play"></i>
-							<p>TailwindCSS</p>
-						</li>
-						<li>
-							<i className="fas fa-play"></i>
-							<p>Styled-Components</p>
-						</li>
-						<li>
-							<i className="fas fa-play"></i>
-							<p>Javascript</p>
-						</li>
-						<li>
-							<i className="fas fa-play"></i>
-							<p>Reactjs & Redux</p>
-						</li>
-						<li>
-							<i className="fas fa-play"></i>
-							<p>Nextjs</p>
-						</li>
-						<li>
-							<i className="fas fa-play"></i>
-							<p>TypeScript</p>
-						</li>
-						<li>
-							<i className="fas fa-play"></i>
-							<p>Nodejs & Expressjs</p>
-						</li>
-						<li>
-							<i className="fas fa-play"></i>
-							<p>MongoDB</p>
-						</li>
-					</ul>
-				</section>
-				<section className={styles.experiences}>
-					<h1>Experiences</h1>
-					<Experiences />
-				</section>
-				<section className={styles.education}>
-					<h1>Education</h1>
-					<Education />
-				</section>
-			</div>
-		);
+	return (
+		<div className={styles.Resume}>
+			<section className={styles.intro}>
+				<header>
+					<h1>Overview</h1>
+					<a href={resume} download>
+						Download Resume
+					</a>
+				</header>
+				<p>
+					Hello{" "}
+					<span aria-label="img" role="img">
+						👋
+					</span>
+					, My name is Precious Origho, a Sofware Developer based in
+					Portharcourt, Rivers State, Nigeria. I enjoy creating the frontend of
+					things that live on the internet as well as building servers they
+					interact with, whether that be websites app, or anything in between. I
+					keep learning new technologies to improve my skillset and build more
+					pixel-perfect, and performant web apps. I'm currently interested in
+					blockchain development and will be exploring it pretty soon.
+				</p>
+				<p style={{ marginTop: "1rem" }}>Technologies I work with currently:</p>
+				<ul>
+					<li>
+						<i className="fas fa-play" /> <p>HTML & CSS(Sass)</p>
+					</li>
+					<li>
+						<i className="fas fa-play" />
+						<p>TailwindCSS</p>
+					</li>
+					<li>
+						<i className="fas fa-play" />
+						<p>Styled-Components</p>
+					</li>
+					<li>
+						<i className="fas fa-play" />
+						<p>Javascript</p>
+					</li>
+					<li>
+						<i className="fas fa-play" />
+						<p>Reactjs & Redux</p>
+					</li>
+					<li>
+						<i className="fas fa-play" />
+						<p>Nextjs</p>
+					</li>
+					<li>
+						<i className="fas fa-play" />
+						<p>TypeScript</p>
+					</li>
+					<li>
+						<i className="fas fa-play" />
+						<p>Nodejs & Expressjs</p>
+					</li>
+					<li>
+						<i className="fas fa-play" />
+						<p>MongoDB</p>
+					</li>
+				</ul>
+			</section>
+			<section className={styles.experiences}>
+				<h1>Experiences</h1>
+				<Experiences />
+			</section>
+			<section className={styles.education}>
+				<h1>Education</h1>
+				<Education />
+			</section>
+		</div>
+	);
 };
 
 export default Resume;
